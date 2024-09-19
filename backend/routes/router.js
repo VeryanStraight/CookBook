93911e56dcec0a1ead0da4b69fa81d141b1facc0
Veryan <veryan.str8@gmail.com>
2024-09-19 12:10:35 +0100

    made a query in the backend and have started to setup the search in the frontend

--- backend/routes/router.js
+++ backend/routes/router.js
@@ -1,8 +1,16 @@
 const express = require('express')
 const router = express.Router()
+const Recipe = require('./models/recipe');
+const Tag = require('./models/tag');
 
-router.get('/recipes', (req, res) => {
-
-})
+router.get('/recipe/search/:name', async (req, res) => {
+    try {
+      const recipes = await Recipe.find({ name: { $regex: req.params.name, $options: 'i' } });
+      res.json(recipes);
+    } catch (err) {
+      res.status(500).json({ message: err.message });
+    }
+  });
+  
 
 module.exports = router
--- frontend/src/components/Search.tsx
+++ frontend/src/components/Search.tsx
@@ -1,14 +1,28 @@
+import { useState } from "react";
+
+const [search, setSearch] = useState("");
+
+const handleSearch = (e: React.FormEvent) => {
+  e.preventDefault();
+};
+
 const Search = () => {
   return (
     <div>
       <form className="d-flex mt-4">
         <input
+          onChange={(e) => setSearch(e.target.value)}
+          value={search}
           className="form-control me-2"
           type="search"
           placeholder="Search"
           aria-label="Search"
         />
-        <button className="btn btn-outline-success" type="submit">
+        <button
+          onClick={handleSearch}
+          className="btn btn-outline-success"
+          type="submit"
+        >
           Search
         </button>
       </form>
