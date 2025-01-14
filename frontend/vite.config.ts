aa07d9e9bb0c53e22a882688d54d2dfe4d09d311
Veryan <veryan.str8@gmail.com>
2025-01-14 12:33:34 +0000

    redeployed

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import fs from 'fs';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base:"/CookBook/"
-})
+  base: '/CookBook/',
+  plugins: [
+    react(),
+    {
+      name: 'copy-404',
+      apply: 'build',
+      writeBundle() {
+        fs.copyFileSync('public/404.html', 'dist/404.html');
+      }
+    }
+  ],
+});
